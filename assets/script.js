a9d54c3c14e1ad65471df847f89a704e17cbc1ee
Giorgia <giorgiamannino@hotmail.com>
2025-01-12 16:44:05 +0100

    Progettosettimanale-S5-L5

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,15 @@ window.addEventListener("scroll", function () {
   const heroSection = document.querySelector(".heroSection");
   const navItem5 = document.querySelector("nav ul li:nth-child(5)");
 
-  // Ottieni la posizione del bordo inferiore dell'header
   const headerBottom = header.getBoundingClientRect().bottom;
 
-  // Ottieni la posizione del bordo inferiore della heroSection
   const heroSectionBottom = heroSection.getBoundingClientRect().bottom;
 
-  // Se la base dell'header tocca la base della heroSection, aggiungi la classe
   if (headerBottom >= heroSectionBottom) {
     header.classList.add("sticky-header");
-    navItem5.classList.add("active"); // Aggiungi classe anche al 5° elemento
+    navItem5.classList.add("active");
   } else {
     header.classList.remove("sticky-header");
-    navItem5.classList.remove("active"); // Rimuovi classe dal 5° elemento
+    navItem5.classList.remove("active");
   }
 });
